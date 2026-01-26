fbf6a06b696eaa67584664b9524a812722ead26f
ohtsuka-shota-se <ohtsuka.se@gmail.com>
2026-01-26 22:59:41 +0900

    changes for v2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Ver 2.0</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -33,3 +33,4 @@ function App() {
 }
 
 export default App
+  
